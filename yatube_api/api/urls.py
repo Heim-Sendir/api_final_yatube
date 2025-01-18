513bcd09b3f78844becc2432cfb302927201e491
Heim_Sendir <alex.noa29@gamil.com>
2025-01-18 12:16:03 +0500

    add urls viewset, auth, jwt

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,20 @@
-from django.urls import path
+from rest_framework import routers
+
+from django.urls import path, include
+
+from .views import PostViewSet, GroupViewSet, FollowViewSet, CommentViewSet
+
+router = routers.DefaultRouter()
+
+router.register('posts', PostViewSet, basename='post')
+router.register('group', GroupViewSet, basename='group')
+router.register('follow', FollowViewSet, basename='follow')
+router.register(r'^posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comment')
+
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,8 +18,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
-    'posts',
+    'djoser',
+    'api.apps.ApiConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,9 +78,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -94,7 +95,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
